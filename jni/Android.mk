ccc19d5f30df3e8331ff5359fcabc2205d4eceef
Sergey Kospanov <sergey.kospanov@gmail.com>
2019-04-03 14:03:57 +0300

    cleanup; add optimization flags

--- jni/Android.mk
+++ jni/Android.mk
@@ -6,7 +6,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := dlib
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../dlib
 LOCAL_SRC_FILES += \
-                ../$(LOCAL_PATH)/../dlib//dlib/threads/threads_kernel_shared.cpp \
+                ../$(LOCAL_PATH)/../dlib/dlib/threads/threads_kernel_shared.cpp \
                 ../$(LOCAL_PATH)/../dlib/dlib/entropy_decoder/entropy_decoder_kernel_2.cpp \
                 ../$(LOCAL_PATH)/../dlib/dlib/base64/base64_kernel_1.cpp \
                 ../$(LOCAL_PATH)/../dlib/dlib/threads/threads_kernel_1.cpp \
@@ -16,6 +16,7 @@ LOCAL_SRC_FILES += \
                 ../$(LOCAL_PATH)/../dlib/dlib/cuda/cpu_dlib.cpp \
                 ../$(LOCAL_PATH)/../dlib/dlib/cuda/tensor_tools.cpp
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
+LOCAL_CFLAGS += -O3 -DNDEBUG -DDLIB_NO_GUI_SUPPORT=on
 
 ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
     LOCAL_ARM_MODE := arm
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,10 +1,6 @@
 NDK_TOOLCHAIN_VERSION := clang
 APP_OPTIM=release
-APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
+APP_ABI := arm64-v8a
 APP_CPPFLAGS := -std=c++11 -frtti -fexceptions
 APP_PLATFORM := android-28
 APP_STL := c++_shared
-#APP_CFLAGS+=-DDLIB_NO_GUI_SUPPORT=on
-#APP_CFLAGS+=-DDLIB_PNG_SUPPORT=off
-APP_CFLAGS+=-DDLIB_JPEG_SUPPORT=on
-APP_CFLAGS+=-DDLIB_JPEG_STATIC=on
